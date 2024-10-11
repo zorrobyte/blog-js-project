d558dd25d12f1df8ade0ffdae3e7b0f653a3c73e
Zorrobyte <ubercomputers@icloud.com>
2024-10-10 23:13:56 -0400

    Initial commit

---
+++ backend/database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  author_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
